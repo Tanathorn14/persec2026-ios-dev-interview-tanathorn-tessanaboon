a15a996e4d0bc3cbcfd0304c3c45274b9ec4f8e4
Tanathorn Tessanaboon <tanathorn@siamsquared.com>
2026-06-23 22:30:16 +0700

    Init project

---
+++ PersecInternviewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersecInternviewTest
+//
+//  Created by Tanathorn Tessanaboon on 23/6/2569 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
