5500c093c3494248ac1295ca18de0fddb6bbdad6
Tanathorn Tessanaboon <tanathorn@siamsquared.com>
2026-06-24 03:12:52 +0700

    Implement first assignment

--- PersecInternviewTest/Assignment/First/FirstAssignmentViewController.swift
+++ PersecInternviewTest/Assignment/First/FirstAssignmentViewController.swift
@@ -9,21 +9,66 @@ import UIKit
 
 class FirstAssignmentViewController: UIViewController {
 
+    static let identifier = "FirstAssignmentViewController"
+
+    required init(viewModel: FirstAssignmentViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: FirstAssignmentViewController.identifier, bundle: Bundle(for: FirstAssignmentViewController.self))
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @IBOutlet var textfield: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    private var viewModel: FirstAssignmentViewModel
+
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setupTextfield()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - UserInterface
+    private func setupTextfield() {
+        self.textfield.delegate = self
+        self.textfield.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
     }
-    */
 
+    // MARK: - Action
+
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            return
+        }
+
+        let isValid = self.viewModel.checkValidBrackets(inputText)
+
+        if isValid {
+            self.resultLabel.text = "True"
+        } else {
+            self.resultLabel.text = "False"
+        }
+    }
+}
+
+extension FirstAssignmentViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if string.isEmpty {
+            self.resultLabel.text = ""
+            return true
+        }
+
+        let allowedCharacters = CharacterSet(charactersIn: "()[]{}")
+        let typedCharacterSet = CharacterSet(charactersIn: string)
+
+        return allowedCharacters.isSuperset(of: typedCharacterSet)
+    }
 }
--- PersecInternviewTest/Assignment/First/FirstAssignmentViewModel.swift
+++ PersecInternviewTest/Assignment/First/FirstAssignmentViewModel.swift
@@ -5,3 +5,30 @@
 //  Created by Tanathorn Tessanaboon on 23/6/2569 BE.
 //
 
+class FirstAssignmentViewModel {
+    init() { }
+
+    func checkValidBrackets(_ input: String) -> Bool {
+        var stack: [Character] = []
+
+        let matchingText: [Character: Character] = [
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        ]
+
+        for char in input {
+            if matchingText.values.contains(char) {
+                stack.append(char)
+            } else if let expectedOpenBracket = matchingText[char] {
+                if stack.isEmpty || stack.last != expectedOpenBracket {
+                    return false
+                }
+                stack.removeLast()
+            }
+        }
+
+        return stack.isEmpty
+    }
+}
+
